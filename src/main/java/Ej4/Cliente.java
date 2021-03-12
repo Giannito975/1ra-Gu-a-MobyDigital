a3b88e0ac9dff186a85fb4060fb9c4043db5528a
Gianni <gricciardi@mobydigital.com>
2021-03-12 09:35:36 -0300

    Desarrollo clase Producto

--- src/main/java/Ej4/Cliente.java
+++ src/main/java/Ej4/Cliente.java
@@ -1,4 +1,19 @@
 package Ej4;
 
-public class Cliente {
+import java.util.Date;
+
+public abstract class Cliente {
+
+    //Atributos de clase.
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento; //En formato de tipo date, uso de día, mes y año obligatorio.
+    private String telefono;
+    private String dni;
+    private String codigo;
+    private float descuento;
+
+
+
+
 }
--- src/main/java/Ej4/Producto.java
+++ src/main/java/Ej4/Producto.java
@@ -1,4 +1,68 @@
 package Ej4;
 
 public class Producto {
+
+    //Atributos
+    private String nombre;
+    private String marca;
+    private float precioUnitario;
+    private int stockActual;
+    private String codigo;
+
+
+    //Metodo constructor
+    public Producto(String nombre, String marca, float precioUnitario, int stockActual, String codigo) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precioUnitario = precioUnitario;
+        this.stockActual = stockActual;
+        this.codigo = codigo;
+    }
+
+    //Getters & Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(float precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getStockActual() {
+        return stockActual;
+    }
+
+    public void setStockActual(int stockActual) {
+        this.stockActual = stockActual;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto: " + getNombre() + "Marca: " + getMarca() + "\nPrecio Unitario: " + getPrecioUnitario()
+                + "Stock actual: " + getStockActual() + "\n\tCodigo: " + getCodigo();
+    }
 }
